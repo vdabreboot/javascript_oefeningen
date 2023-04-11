fd48afd161ee7b60d92077b963c83c6db80f937d
Peter Kuda <peter.kuda@gmail.com>
2023-04-11 15:54:56 +0200

    Snake background

--- snake.js
+++ snake.js
@@ -1,23 +1,31 @@
 "use strict";
 
 let spelBord;
+let snake;
 
-async function createLayout(width=10){
-    //const div = document.getElementById("snake");
-    const spelBord= document.getElementById("snake");
-    //spelBord = document.createElement("table");
+async function createLayout(width=10){    
+    const spelBord= document.getElementById("snake");    
     for(let row=0 ; row<width;row++){
         const tr = document.createElement("div");
         tr.classList.add("row");
         for(let column=0; column < width;column++){
             const td = document.createElement("div");
+            td.id = `tile_${row}-${column} `;
             td.innerText = " ";
-            td.classList.add("column");            
+            td.classList.add("column");                        
             tr.appendChild(td);         
         }                
         spelBord.appendChild(tr);        
-    }
-    div.appendChild(spelBord);
+    }    
+}
+async function init(){
+    await(createLayout());
+    snake = { x: Math.floor(Math.random()*10), y: Math.floor(Math.random()*10)};
+    console.log(snake);
+    console.log(`tile_${snake.x}-${snake.y}`);
+    const divSnakeHead = document.getElementById(`tile_${snake[0]}-${snake[1]}`);
+    divSnakeHead.classList.add("snake");
 }
 
-createLayout();
+/*createLayout();*/
+init();
